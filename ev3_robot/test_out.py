409227a116de2c90e32f9587acc58a75938231cd
monstertau <trungdungvu172@gmail.com>
2022-01-12 22:15:28 +0700

    cleaning up

--- ev3_robot/test_out.py
+++
@@ -1,8 +0,0 @@
-from ev3dev2.motor import *
-from ev3dev2.sound import Sound
-from ev3dev2.sensor import *
-from ev3dev2.sensor.lego import *
-
-motorC = MediumMotor(OUTPUT_C)
-
-motorC.run_to_rel_pos(position_sp=360, speed_sp=500)
